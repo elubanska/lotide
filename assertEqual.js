6c24cca0f44fcebf0ebb7ad192c25e0cc48ba776
Emilia <e.lubanska@gmail.com>
2021-05-25 01:11:08 +0000

    commit assert file

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected){
-    console.log("Assertation Passed: " + actual +" === "+expected+ " 😀")
+    console.log(`Assertation Passed: ${actual} === ${expected} 😀`)
   } else {
-    console.log("Assertation Failed: " + actual +" !== "+expected+ " 🥴")
+    console.log(`Assertation Failed: ${actual} !== ${expected} 🥴`)
   }
 };
 
