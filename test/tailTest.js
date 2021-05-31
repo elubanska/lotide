895f88488441adf8b35acf5fe750ac77cb1f5e7b
Emilia <e.lubanska@gmail.com>
2021-05-31 16:44:16 -0400

    adding Mocha testing to tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,22 @@
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-console.log("--------");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-console.log("--------");
-assertEqual(tail(words),['Lighthouse', 'Labs']);
-console.log("--------");
-const test = [1,2,7,4,5];
-assertEqual(tail(test),[2,3,4,5]);
+// console.log("--------");
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words));
+// console.log("--------");
+// assertEqual(tail(words),['Lighthouse', 'Labs']);
+// console.log("--------");
+// const test = [1,2,7,4,5];
+// assertEqual(tail(test),[2,3,4,5]);
+
+describe("#tail", () => {
+    it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+        assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']); 
+    });
+
+    it("returns [6, 7] for [3, 6, 7]", () => {
+        assert.deepEqual(tail([3, 6, 7]), [6, 7]); 
+    });
+});
