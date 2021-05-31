5424f3cc6eecab799f62a56266cf00a10fdc761f
Emilia <e.lubanska@gmail.com>
2021-05-31 15:03:36 -0400

    drying up assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,9 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
-  // if (actual === expected){
-  //   console.log(`Assertation Passed: ${actual} === ${expected} 😀`)
-  // } else {
-  //   console.log(`Assertation Failed: ${actual} !== ${expected} 🥴`)
-  // }
+  
   eqArrays(actual,expected);
 };
 
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    console.log(`Assertation Failed: ${actual} !== ${expected} 🥴`)
-    return false;
-  } 
-  for (let i = 0; i < actual.length; i++){
-    if (actual[i] !== expected[i]) {
-      console.log(`Assertation Failed: ${actual} !== ${expected} 🥴`)
-      return false;
-    }   
-  }
-  console.log(`Assertation Passed: ${actual} === ${expected} 😀`)
-  return true;
-};
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-//assertArraysEqual(eqArrays(["1","2","3"], ["1",2,"3"]), true);
+module.exports = assertArraysEqual;
